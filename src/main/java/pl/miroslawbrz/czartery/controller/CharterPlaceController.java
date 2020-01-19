11ada9b20c8f333d3d23a5a2728c2f519e394682
MiroslawBrz <miroslawbrzezinski@gmail.com>
2020-01-19 17:17:12 +0100

    CharterPlace controller

--- src/main/java/pl/miroslawbrz/czartery/controller/CharterPlaceController.java
+++ src/main/java/pl/miroslawbrz/czartery/controller/CharterPlaceController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import pl.miroslawbrz.czartery.api.request.CreateCharterPlaceRequest;
 import pl.miroslawbrz.czartery.api.response.CharterPlaceResponse;
+import pl.miroslawbrz.czartery.model.CharterPlace;
+import pl.miroslawbrz.czartery.model.CharterPlaceAddress;
 import pl.miroslawbrz.czartery.service.CharterPlaceService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/api/charterplace")
@@ -25,7 +27,38 @@ public class CharterPlaceController {
     @PostMapping(produces = "application/json")
     public ResponseEntity<CharterPlaceResponse> addNewCharterPlaceWithFullAddress(
             @RequestBody CreateCharterPlaceRequest request){
-        return null;
+        return charterPlaceService.createCharterPlace(request);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CharterPlace> getCharterPlaceById(@PathVariable Long id){
+        return charterPlaceService.getCharterPlace(id);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CharterPlace>> getAllCharterPlaces(){
+        return charterPlaceService.getAllCharterPlaces();
+    }
+
+    @PutMapping(value = "/{id}/address",produces = "application/json")
+    public ResponseEntity<CharterPlaceResponse> updateCharterPlaceAddress(
+            @PathVariable Long id,
+            @RequestBody CharterPlaceAddress address){
+        return  charterPlaceService.updateCharterPlaceAddress(id, address);
+    }
+
+    @PutMapping(value = "/{id}", produces = "application/json")
+    public ResponseEntity<CharterPlaceResponse> updateCharterPlace(
+            @PathVariable Long id,
+            @RequestBody CreateCharterPlaceRequest request
+    ){
+        return charterPlaceService.updateCharterPlaceData(id, request);
+    }
+
+    @DeleteMapping(value = "/{id}", produces = "application/json")
+    public ResponseEntity<CharterPlaceResponse> deleteCharterPlace(@PathVariable Long id){
+        return charterPlaceService.deleteCharterPlace(id);
+    }
+
 
 }
---
+++ src/test/java/pl/miroslawbrz/czartery/CharterPlaceTests.java
@@ -0,0 +1,8 @@
+package pl.miroslawbrz.czartery;
+
+public class CharterPlaceTests {
+
+    //todo integration tests
+    //todo sql tables for address, charter place, user roles
+
+}
